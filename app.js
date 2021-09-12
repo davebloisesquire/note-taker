85099a81b8d50234ef5fd14d289e59eaaea68398
theDaveB <davebloisesquire@gmail.com>
2021-09-11 20:40:20 -0700

    Capitalization is important

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const util = require("util");
 // Setup variable objects
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({
